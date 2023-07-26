d166e457611d7ccd8c229971786e769250df1245
rsk-ymst <jey.togo@gmail.com>
2023-07-26 14:19:23 +0900

    first commit

---
+++ src/app/buttle/page.tsx
@@ -0,0 +1,43 @@
+import Image from 'next/image'
+
+const GameWindow = () => {
+  return (
+    <div className='flex justify-center m-4'>
+      <div className='bg-black w-[960px] h-[540px]'>
+        <div className=' flex justify-center flex-col items-center'>
+            <div className='bg-blue-600 w-[920px] h-[50px] mt-2 rounded-lg'>
+
+            </div>
+          {/* </div> */}
+
+          {/* バトルフィールド */}
+          {/* <div className='flex justify-center'> */}
+            <div className='bg-gray-700 w-[920px] h-[320px]'>
+              {/* enemy filed */}
+              {/* nakama filed */}
+            </div>
+          {/* </div> */}
+
+          {/* コマンドメニュー */}
+          {/* <div className='flex justify-center m-8'> */}
+            <div className='bg-blue-600 w-[920px] h-[150px] mb-2 rounded-lg'>
+              <div className='flex'>
+                <div className='flex-1 h-full border-2 border-t-red-50'>
+                  <div>たたかう</div>
+                  <div>にげる</div>
+                  <div>どうぐ</div>
+                  <div>助ける</div>
+                </div>
+
+                <div className='flex-1 h-full border-2 border-t-red-50'>
+                  ほげ
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default GameWindow
