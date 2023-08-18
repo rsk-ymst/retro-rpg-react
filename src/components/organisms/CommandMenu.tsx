d35ff490fdc37832d06a6d64b78f071d03972df3
rsk-ymst <jey.togo@gmail.com>
2023-08-18 19:31:54 +0900

    [modify] スキル・道具リストの表示を可能に

--- src/components/organisms/CommandMenu.tsx
+++ src/components/organisms/CommandMenu.tsx
@@ -4,7 +4,7 @@ import { useContext } from 'react'
 import BasicOptions from '../molecules/BasicOptions'
 import EnemyInfo from '../molecules/EnemyInfo'
 import UserInfo from '../molecules/UserInfo'
-import { Context } from '@/game/context'
+import { Context, UIFocusStatus } from '@/game/context'
 
 export type Props = {
   className: string
@@ -18,8 +18,24 @@ const CommandMenu = ({ className }: Props) => {
     <div
       className={`flex bg-gradient-to-b from-blue-500 to-blue-700 rounded-lg border-2 border-t-red-50 ${className}`}
     >
-      <BasicOptions className='flex-[0.8] h-full' />
-      <EnemyInfo className='flex-1 h-full border-x-2' UIFocus={context?.UIFocus} />
+      {(context.UIFocus === UIFocusStatus.BASIC_OPTIONS ||
+        context.UIFocus === UIFocusStatus.ENEMY_INFO) && (
+        <>
+          <BasicOptions className='flex-[0.8] h-full' />
+          <EnemyInfo className='flex-1 h-full border-x-2' UIFocus={context?.UIFocus} />
+        </>
+      )}
+      {context.UIFocus === UIFocusStatus.SKILLS && (
+        <>
+          <BasicOptions className='flex-[1.8] h-full' />
+        </>
+      )}
+      {context.UIFocus === UIFocusStatus.ITEM_LIST && (
+        <>
+          <BasicOptions className='flex-[1.8] h-full' />
+        </>
+      )}
+
       <UserInfo className='flex-[2] h-full border-t-red-50'></UserInfo>
     </div>
   )
